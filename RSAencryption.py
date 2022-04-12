72d27e675340e573e0c883d77aa97661712e0ec8
0TheRealFish0 <finnwfreeman@icloud.com>
2022-04-12 14:30:23 +0200

    changes

--- RSAencryption.py
+++ RSAencryption.py
@@ -2,16 +2,22 @@ from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES, PKCS1_OAEP
 
+# creating private key
+
 key = RSA.generate(2048)
 private_key = key.export_key()
 file_out = open("private.pem", "wb")
 file_out.write(private_key)
 file_out.close()
 
+# creating public key
+
 public_key = key.publickey().export_key()
 file_out = open("receiver.pem", "wb")
 file_out.write(public_key)
 file_out.close()
+
+# encrypting data
 
 data = "I met aliens in UFO. Here is the map.".encode("utf-8")
 file_out = open("encrypted_data.bin", "wb")
